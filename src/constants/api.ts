0d17a69f379f43f74e4b553114e559ddfa197ab5
amirsarfraz50 <amirsarfraz50@gmail.com>
2024-08-20 12:13:32 +0500

    camera and video fixed

---
+++ src/constants/api.ts
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+export const REQUEST_HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
+
+export const API_URL = '';
+export const API_DoAAdHAAROCR = '';
+export const API_UploadVCIPRecordedVideo = '';
+export const API_GetVcipLinkDetails = '';
+export const NODE_ENV = '';
